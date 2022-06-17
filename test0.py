7e84e38b788e17222e94f138935866737c59c4a5
zhusheldon <sheldonzhu@ymail.com>
2022-06-17 10:09:05 -0500

    Initial commit

---
+++ test0.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 16 21:12:08 2022
+
+@author: sheld
+"""
+import sys
+import os
+import numpy as np
+from time import time
+
+class FcnFamily():
+    pass
+
+print(dir(FcnFamily))
